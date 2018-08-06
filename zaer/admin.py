9b950fc5780d75741ebb89ab6722cb7f31e2c626
sahama <sayyid.hamid.mahdavi@gmail.com>
2018-08-06 16:13:27 +0000

    jalali in admin view

--- zaer/admin.py
+++ zaer/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Zaer
+from jalali_date.admin import ModelAdminJalaliMixin
+from jalali_date.fields import JalaliDateField, SplitJalaliDateTimeField
+from django.forms import DateTimeField, DateField
 
-# Register your models here.
-admin.site.register(Zaer)
+@admin.register(Zaer)
+class ZaerModelAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    formfield_overrides = {
+        DateField: {'widget': JalaliDateField},
+        # DateTimeField: {'widget': SplitJalaliDateTimeField},
+    }
 
+# admin.site.register(ZaerModelAdmin)
