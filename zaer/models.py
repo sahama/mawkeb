97c5be676497778ab8ad3a3aee20770ccafc7e54
sahama <sayyid.hamid.mahdavi@gmail.com>
2018-09-29 17:04:34 +0000

    auto out in 3 days

--- zaer/models.py
+++ zaer/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.utils.timezone import now
+import datetime
 
 class Zaer(models.Model):
+    GENDER_CHOICES = (('m', 'مرد'),
+                      ('f', 'زن'))
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     address = models.CharField(max_length=1000, blank=True)
@@ -10,5 +13,10 @@ class Zaer(models.Model):
     age = models.IntegerField(blank=True, default=0)
     in_datetime = models.DateTimeField(default=now, blank=True, db_index=True)
     out_datetime = models.DateTimeField(blank=True, db_index=True, null=True)
-    gender = models.CharField(max_length=1, blank=True, default='m')
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1, blank=True, default='m')
     image = models.ImageField(upload_to='images/', blank=True, default='no_image')
+
+    def save(self, *args, **kwargs):
+        self.out_datetime = self.in_datetime + datetime.timedelta(days=3)
+        super(Zaer, self).save(*args, **kwargs)
+
